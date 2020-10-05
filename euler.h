02d456ce6393610b3b329cc42a21b6e7c9325528
Kristoff1318 <kristoff.misquitta@gmail.com>
2020-10-05 02:03:26 -0400

    add makefile and dependencies

---
+++ euler.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int problem5();
+int problem6();
+int problem8();
---
+++ main.c
@@ -0,0 +1,7 @@
+#include "euler.h"
+
+int main() {
+    printf("%d\n", problem5());
+    printf("%d\n", problem6());
+    printf("%d\n", problem8());
+}
